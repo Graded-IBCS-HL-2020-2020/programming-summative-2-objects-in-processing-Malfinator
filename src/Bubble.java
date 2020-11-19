668833b537a4a617bce47beb6f70de6482df6448
David Griswold <david.griswold@graded.br>
2020-11-19 13:30:23 +0000

    handing in

--- src/Bubble.java
+++ src/Bubble.java
@@ -11,7 +11,43 @@ class Bubble {
     private int borderColor; 
     private float speedY;
     private float speedX;
+     public Bubble(Sketch sketch) {
+        s = sketch;
+        diameter = s.random(10, 20);
+        x = s.random(diameter / 2, s.width - diameter / 2);
+        y = s.random(diameter / 2, s.height - diameter / 2);
+        col = s.color(255, 150);
+        speedY = s.random(-10, -20);
+        speedX = 0;
 
+     }
+
+      public Bubble(Sketch sketch, float diam, float X, float Y, int cor, int borderCor, float spdY, float spdX) {
+          s = sketch;
+          diameter = diam;
+          x = X;
+          y = Y;
+          col = cor;
+          borderColor = borderCor;
+          speedY = spdY;
+          speedX = spdX;
+
+
+     }
+      public Bubble(Sketch sketch, float diam, float X, float Y, int borderCor, float spdX) {
+        s = sketch;
+        diameter = diam;
+        x = X;
+        y = Y;
+        col = s.color(255, 150);
+        speedY = s.random(-10, -20);
+        speedX = 0;
+      }
+
+        
+
+
+    
     /**
      * SUMMATIVE REQUIRED Add two constructors for the Bubble class, one that is
      * mostly default and one that allows custom values for whichever you want to be
@@ -28,7 +64,10 @@ class Bubble {
      * SUMMATIVE OPTIONAL Add more than two constructors for varying levels of
      * customization
      */
-
+    
+     public float getRadius() {
+        return diameter / 2;
+    }
     /*
      * SUMMATIVE REQUIRED Add a method called `getRadius()` that returns a float
      * representing the radius of the bubble
@@ -43,18 +82,23 @@ class Bubble {
 
     /** Moves the balls. */
     public void moveBubble() {
+    
 
         /*
          * SUMMATIVE OPTIONAL Tweak this method so that the bubbles don't bounce off the
          * left and right sides, but loop around to the other side instead.
          */
-
+         if (x < (-getRadius())) {
+             x = s.width + getRadius();
+         }
+         
+         if (x > (s.width + getRadius())) {
+             x = -getRadius();
+         }
         /* SUMMATIVE OPTIONAL Tweak bubble movement so that it moves more randomly */
 
         /* If the bubble is on the left or right edge, bounce! */
-        if (x > (s.width - getRadius()) || x < getRadius()) {
-            speedX = -speedX;
-        }
+        
         /* If the ball goes off the top, move it to the bottom. */
         if (y < (-getRadius())) {
             y = s.height + getRadius();
@@ -68,3 +112,4 @@ class Bubble {
 
     }
 }
+//Made by Malfinator in collaboration with G r i s w o l d, creating java programs which increase my self-esteem since 2020
